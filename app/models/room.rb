6e46382dbbae43856369038427e0920be8e01fa9
ksaitok <kazuma.saito@gmail.com>
2013-01-06 14:21:17 +0900

    antes de mudar a parte room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,8 @@
 class Room < ActiveRecord::Base
   attr_accessible :description, :location, :title
 
+  validates_presence_of :description, :location, :title
+
   def complete_name
   	"#{title}, #{location}"
   end
